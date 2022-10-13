a0e7fa1e97a741843cce608163cb31da31650d20
wilfrid beaunes <kesnelbeaunes@gmail.com>
2022-10-13 18:08:15 +0200

    front quittance sang signature

--- frontend/src/app/invoice/invoice.component.ts
+++ frontend/src/app/invoice/invoice.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit,ViewChild,ElementRef  } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-invoice',
@@ -6,6 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoice.component.css']
 })
 export class InvoiceComponent implements OnInit {
+
+  @ViewChild('htmlData') htmlData!: ElementRef;
+  USERS = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'sincere@april.biz',
+      phone: '1-770-736-8031 x56442',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'shanna@melissa.tv',
+      phone: '010-692-6593 x09125',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      email: 'nathan@yesenia.net',
+      phone: '1-463-123-4447',
+    },
+  ];
+
+  public openPDF(): void {
+    let DATA: any = document.getElementById('htmlData');
+      html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('angular-demo.pdf');
+    });
+  }
 
   constructor() { }
 
