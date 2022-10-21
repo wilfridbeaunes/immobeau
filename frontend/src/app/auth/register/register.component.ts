b5ba337c1602c2c29a255bebe7eabdc2d2fd2647
wilfrid beaunes <kesnelbeaunes@gmail.com>
2022-10-21 10:00:49 +0200

    archi changed

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,54 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http'
+import { EmailExist } from '../../helpers/email.validator';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
 })
-export class LoginComponent implements OnInit {
+export class RegisterComponent implements OnInit {
 
   myForm!: FormGroup; 
   personalSubmitted = false;
+  role='propritaire'; //default value for the role
+
   constructor(
     private fb: FormBuilder,
     private http: HttpClient,
     private router: Router,
     private _snackBar: MatSnackBar,
-    private AuthService: AuthService
-    ) { }
-
+    private AuthService: AuthService) { }
+  
   ngOnInit(): void {
     //name of fields i want to get the values 
     this.myForm = this.fb.group({
+      firstName: ['', Validators.required], 
+      lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
+      role:''
+    }, {
+      validator: [ EmailExist('email', this.http)],
     });
+  }
+
+ //change if the user is a propritaire or a locataire
+ //get user role
+  onChange(e:any) {
+    this.role= e.target.value;
   }
 
   //get the form control
   get personal() { return this.myForm.controls; }
 
-  //user connextion 
-  submit(){
+  //submit the personal form
+  submitPersonal() {
     this.personalSubmitted = true;
     if (!this.myForm.invalid) {
       const formData = this.myForm.getRawValue();
       const data = {
         email: formData.email,
         password: formData.password,
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        role: this.role
       }
+      console.log(data);
       //send the data to the backend to create a new user
-      this.AuthService.loginUser(data).then((res: any) => {
+      this.AuthService.registerUser(data).then((res: any) => {
         console.log(res);
         if (res.success) {
-          this.router.navigate(['/profil']);
-          // get information
-          this._snackBar.open('Connexion succès', 'Close', {duration: 1500,  panelClass: ['green-snackbar']});
+          this.router.navigate(['/login']);
+          this._snackBar.open('Compte crée avec succès', 'Close', {duration: 1500,  panelClass: ['green-snackbar']});
         }else {
           this._snackBar.open(res.message, 'Close', {duration: 1500,  panelClass: ['red-snackbar']});
           };
@@ -59,7 +76,5 @@ export class LoginComponent implements OnInit {
       });
 
     }
-
   }
-
 }
